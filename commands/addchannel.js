8294fbd74fe6810423b92fe86959413d084d0eab
DarkSnakeX <darksnakex12@gmail.com>
2023-05-27 21:58:17 +0200

    Permissions required
    Administrator required for users to use these commands.

--- commands/addchannel.js
+++ commands/addchannel.js
@@ -1,11 +1,16 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { addChannel } = require('../database.js');
+const { PermissionsBitField } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('addchannel')
 		.setDescription('Adds the channel to analyze'),
 	async execute(interaction) {
+		if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+			await interaction.reply('No tienes permisos de administrador para ejecutar este comando.');
+			return;
+		}
         const channelId = interaction.channel.id;
         const res = await addChannel(channelId);
 		if(res == 0){
--- commands/removechannel.js
+++ commands/removechannel.js
@@ -1,11 +1,16 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { removeChannel } = require('../database.js');
+const { PermissionsBitField } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('removechannel')
 		.setDescription('Remove the channel to analyze'),
 	async execute(interaction) {
+		if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+			await interaction.reply('No tienes permisos de administrador para ejecutar este comando.');
+			return;
+		}
         const res = await removeChannel(interaction.channel.id);
 		if(res == 0){
 			await interaction.reply('Channel is already removed');
